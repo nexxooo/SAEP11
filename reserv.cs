c3e9a24253e52350d6181f57fc356dc34d30e6d0
nexxooo <fabiolongofabio@gmail.com>
2025-11-18 20:38:12 +0100

    horaire sous fichier

--- reserv.cs
+++ reserv.cs
@@ -4,38 +4,85 @@ using System.Collections.Generic;
 
 class SAE
 {
-	static void Main()
-	{
+    static void Main()
+    {
+        //test
+        string[] temp = Extraire(1, 15);
+        afficherHoraire(temp);
 
-	}
-	struct Traverse
-	{
-		public string nom;
-		public int idLiaison;
-		public string date;
-		public string heure;
-		public string horodatage;
+    }
+    struct Traverse
+    {
+        public string nom;
+        public int idLiaison;
+        public string date;
+        public string heure;
+        public string horodatage;
 
-	}
-	struct Passager
-	{
-		public Dictionary<string,string> passager;
+    }
+    struct Passager
+    {
+        public Dictionary<string, string> passager;
 
-	}
-	struct Vehicule
-	{
-	    public string codeCategorie;
-	    public int quantite;
+    }
+    struct Vehicule
+    {
+        public string codeCategorie;
+        public int quantite;
 
-	}
-	struct Reservation
-	{
-		public Traverse Traversé;
-		public Passager[] passager;
-		public véhicules[] vehicule;
+    }
+    struct Reservation
+    {
+        public Traverse Traversé;
+        public Passager[] passager;
+        public Vehicule[] vehicule;
 
 
-	}
+    }
+    static string[] Extraire(int id, int jour)
+    {
 
-    
+        string[] temp =null;
+        string nomFichier = "";
+        switch (id) //associ id avec le bon nom de fichier
+        {
+            case 1:
+                nomFichier = "Lorient_groix.csv";
+                break;
+            case 2:
+                nomFichier = "Groix_lorient.csv";
+                break;
+            case 3:
+                nomFichier = "Quiberon_palais.csv";
+                break;
+            case 4:
+                nomFichier = "Palais_quiberon.csv";
+                break;
+            default:
+                break;
+        }
+
+        // on ouvre le fichier
+        using (FileStream fs = new FileStream(nomFichier, FileMode.Open, FileAccess.Read))
+        using (StreamReader horaire = new StreamReader(fs))
+        {
+            int i = 1;
+            string ligne;
+	//on parcour le fichier jusqu'a que l'on arrive au bon jour(num ligne)
+            while (!horaire.EndOfStream && i <= jour)
+            {
+                ligne = horaire.ReadLine();
+                temp = ligne.Split(",");
+                i++;
+            }
+        }
+        return temp;
+    }
+    static void afficherHoraire(string[] tab)
+    {
+        foreach (var item in tab)
+        {
+            Console.WriteLine(item);
+        }
+    }
 }
