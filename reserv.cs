00968a5b57733a44112eb9fdb15445b915d31f33
Nexxo <fabiolongofabio@gmail.com>
2025-11-18 19:24:08 +0100

    debut structure

--- reserv.cs
+++ reserv.cs
@@ -2,7 +2,40 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
-class test
+class SAE
 {
+	static void Main()
+	{
+
+	}
+	struct Traverse
+	{
+		public string nom;
+		public int idLiaison;
+		public string date;
+		public string heure;
+		public string horodatage;
+
+	}
+	struct Passager
+	{
+		public Dictionary<string,string> passager;
+
+	}
+	struct Vehicule
+	{
+	    public string codeCategorie;
+	    public int quantite;
+
+	}
+	struct Reservation
+	{
+		public Traverse Traversé;
+		public Passager[] passager;
+		public véhicules[] vehicule;
+
+
+	}
+
     
 }
