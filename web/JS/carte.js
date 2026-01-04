ed535c8322e24b9740f428fb3463258f2a028431
Nexxo <fabiolongofabio@gmail.com>
2026-01-04 17:59:11 +0100

    commentaire

--- web/JS/carte.js
+++ web/JS/carte.js
@@ -6,16 +6,16 @@ async function valider() {
 	modif(reserv, data)
 
 }
-function modif(reserv, data) {
+function modif(reserv, data) { //fonction pricipale 
 	changeGauche(data)
-	nb = data.nbPassagers + data.nbVehicules - 1
+	nb = data.nbPassagers + data.nbVehicules - 1 //une carte est deja présente sur la page mais elle est cacher 
 
 	ajoutCarte(nb);
 	modiDroitPassager(reserv.value, data.nbPassagers)
 	modiDroitVehicule(reserv.value, data.nbVehicules, nb)
 
 
-	element = document.querySelector(".card").classList.toggle("cacher")
+	element = document.querySelector(".card").classList.toggle("cacher") //on chacher toute les carte sauf la première
 }
 
 async function infoReserv(reserv) {
@@ -31,11 +31,11 @@ async function infoReserv(reserv) {
 	}
 }
 
-function changeGauche(data) {
+function changeGauche(data) { //modifie la partie gauche des carte (partie commune a toute)
 	const element = document.querySelectorAll(".left-section .value");
 
 	const element2 = document.querySelectorAll(".reservation-info .value")
-
+	//diffrente partie a modifier (toujours les meme)
 	element[0].innerHTML = data.portDepart
 	element[1].innerHTML = data.portArrivee
 	element[2].innerHTML = data.date
@@ -46,7 +46,7 @@ function changeGauche(data) {
 
 }
 
-function ajoutCarte(nb) {
+function ajoutCarte(nb) { //ajoute le bon nombre de carte nbPassager +nbVehicule
 	const main = document.querySelector("main");
 	const carte = document.querySelector(".card");
 
@@ -59,7 +59,7 @@ function ajoutCarte(nb) {
 
 }
 
-async function infoPassager(reserv, id) {
+async function infoPassager(reserv, id) { //recupere les info d'un passager
 	const api = `https://can.iutrs.unistra.fr/api/reservation/${reserv}/passager/${id}`;
 
 	try {
@@ -72,19 +72,19 @@ async function infoPassager(reserv, id) {
 	}
 }
 
-async function modiDroitPassager(reserv, nb) {
+async function modiDroitPassager(reserv, nb) { // modifie la partie droite si passager (nb = nombre passager)
 	const listeCarte = document.querySelectorAll(".card")
 
-	for (let index = 1; index <= nb; index++) {
-		data = await infoPassager(reserv, index);
-		const carteEncour = listeCarte[index - 1].querySelectorAll(".passenger-info .value")
+	for (let index = 1; index <= nb; index++) { //on commence toujours a remplir les première carte par les passager
+		data = await infoPassager(reserv, index); // recupere les info du passager
+		const carteEncour = listeCarte[index - 1].querySelectorAll(".passenger-info .value") // -1 a cause du décallager entre id du passager et sa place dans la liste
 		carteEncour[0].innerHTML = data.nom
 		carteEncour[1].innerHTML = data.prenom
 		carteEncour[2].innerHTML = data.libelleCategorie
 		carteEncour[3].innerHTML = data.price + " €"
 	}
 }
-async function infoVehicule(reserv, id) {
+async function infoVehicule(reserv, id) { //recuprer les info pour 1 véhicule
 	const api = `https://can.iutrs.unistra.fr/api/reservation/${reserv}/vehicule/${id}`;
 
 	try {
@@ -101,8 +101,10 @@ async function modiDroitVehicule(reserv, nb, max) {
 
 	for (let index = 1; index <= nb; index++) {
 		data = await infoVehicule(reserv, index);
+		//on change les carte après celle des passager 
 		const carteEncour = listeCarte[((max + 1) - index)].querySelectorAll(".passenger-info")
 
+		//les carte véhicule on moins de colonne donc on remplace toute cette partie
 		carteEncour[0].innerHTML = `<tr>
                                 <td class="label">Catégorie</td>
 			<td class="value">${data.libelle}</td>
@@ -118,6 +120,7 @@ async function modiDroitVehicule(reserv, nb, max) {
 
 
 	}
+	// on modifie aussi le titre
 	for (let index = 1; index <= nb; index++) {
 		data = await infoVehicule(reserv, index);
 		const carteEncour = listeCarte[((max + 1) - index)].querySelectorAll(".passenger-header #titre-droit")
@@ -126,22 +129,23 @@ async function modiDroitVehicule(reserv, nb, max) {
 	}
 }
 
-index = 0
+index = 0 // carte actuelle
 function suivant() {
 	const listCarte = document.querySelectorAll(".card");
 	index += 1
-	if (index < 0) { index = 0 }
+	if (index < 0) { index = 0 } //on verif que index ne depasse pas les limte
 	if (index < listCarte.length) {
 		listCarte.forEach(element => {
-			if (!element.classList.contains("cacher")) {
+			if (!element.classList.contains("cacher")) { //on chache toute les carte
 				element.classList.toggle("cacher")
 			}
 		});
 
-		listCarte[index].classList.toggle("cacher")
+		listCarte[index].classList.toggle("cacher") // on revele la bonne carte
 
 	}
 }
+//meme fonction
 function precedent() {
 	const listCarte = document.querySelectorAll(".card");
 	index -= 1
--- web/JS/tableaux.js
+++ web/JS/tableaux.js
@@ -1,18 +1,18 @@
 async function validez() {
-	let date = document.getElementById("date");
-	let destination = document.getElementById("destination")
+	let date = document.getElementById("date"); //on recupere la date
+	let destination = document.getElementById("destination") //on recuper la destination
 	//let selectelement = document.querySelector("main")
 
 	//console.log(date.value)
 	//console.log(destination.value)
 
 	//selectelement.innerHTML += `test`
-	data = await info(destination.value, date.value);
+	data = await info(destination.value, date.value); //on recupere les info des traverse (heure etc)
 	//console.log(data[0].heure)
-	placement(data)
+	placement(data) //fonction qui placera toutes les carte avec les info des différente traversé 
 }
 
-async function info(id, date) {
+async function info(id, date) { //on recupere les info
 	const API = `https://can.iutrs.unistra.fr/api/liaison/${id}/remplissage/${date}`
 	try {
 		const reponse = await fetch(API);
@@ -27,9 +27,9 @@ async function info(id, date) {
 
 function placement(data) {
 	let selectMain = document.querySelector("main");
-	selectMain.innerHTML = ""
+	selectMain.innerHTML = "" //on enleve toute les carte 
 
-	data.forEach(element => {
+	data.forEach(element => { // on regarde le taux de remplissage pour mettre la bonne couleur qui sera une classe dans le css
 
 		capa = element.nbReservationVoitures;
 		capamax = element.capaciteVoitures;
@@ -44,7 +44,7 @@ function placement(data) {
 			code = "rouge"
 		}
 
-
+// on place les carte avec la couleur (classe) et toute les info
 		selectMain.innerHTML += `<section class="${code}" > <p>heure:${element.heure}</p>
 <p>passager: ${element.nbReservationPassagers}/${element.capacitePassagers}</p>
 <p>vehicule: ${element.nbReservationVoitures}/ ${element.capaciteVoitures}</p> 
